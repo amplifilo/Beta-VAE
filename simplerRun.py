96bb1df5b04ca5e030b26839838e141333db4079
amplifilo <68553007+amplifilo@users.noreply.github.com>
2022-06-18 18:04:51 -0400

    fixed .item

--- simplerRun.py
+++ simplerRun.py
@@ -259,7 +259,7 @@ class Solver(object):
 
                 if self.global_iter%self.display_step == 0:
                     pbar.write('[{}] recon_loss:{:.3f} total_kld:{:.3f} mean_kld:{:.3f}'.format(
-                        self.global_iter, recon_loss.data[0], total_kld.data[0], mean_kld.data[0]))
+                        self.global_iter, recon_loss.data.item(), total_kld.data.item(), mean_kld.data.item()))
 
                     var = logvar.exp().mean(0).data
                     var_str = ''
